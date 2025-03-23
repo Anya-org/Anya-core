6b2543e256fca33fbacbd524774672b6629b7ec3
bo_thebig <botshelomokoka@gmail.com>
2025-03-23 19:43:03 +0200

    [AIR-3][AIS-3][BPC-3] Update network component validation to include BPC-3 label
    - Modified the label validation logic for Network category components to require the BPC-3 label in addition to existing labels.
    - Ensured compliance with updated AI labeling standards.
    
    Signed-off-by: bo_thebig <botshelomokoka@gmail.com>

--- src/ai/label_checker.rs
+++ src/ai/label_checker.rs
@@ -2,7 +2,7 @@ impl LabelValidator {
     pub fn validate_component(&self, component: &Component) -> Result<()> {
         let required_labels = match component.category {
             ComponentCategory::Consensus => vec!["BPC-3", "RES-3"],
-            ComponentCategory::Network => vec!["AIS-3", "SCL-3"],
+            ComponentCategory::Network => vec!["AIS-3", "SCL-3", "BPC-3"],
             ComponentCategory::SmartContract => vec!["AIT-3", "BPC-3"],
             ComponentCategory::CrossChain => vec!["RES-3", "SCL-3"],
         };
