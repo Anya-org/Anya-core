85a17c569574138e9a3ea26b9beb0951b040b8cd
botshelomokoka <botshelomokoka@gmail.com>
2025-03-28 16:22:35 +0000

    Add CLI implementation with clap command structure

---
+++ cli/src/lib.rs
@@ -0,0 +1,12 @@
+//! cli crate
+//! [AIR-3][AIS-3][BPC-3][AIP-3][RES-3]
+
+/// Placeholder module for cli
+pub mod cli {
+    /// Returns the version of this crate
+    pub fn version() -> &'static str {
+        "0.1.0"
+    }
+}
+
+pub use cli::version;
