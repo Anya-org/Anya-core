4448cacdd75de35b2326a945a848254753a62933
bo_thebig <botshelomokoka@gmail.com>
2025-05-29 11:07:59 +0000

    test: Update protocol and Web5 integration tests

--- tests/web5_integration.rs
+++ tests/web5_integration.rs
@@ -2,10 +2,9 @@
 fn test_web5_did_rotation() {
     let mut did = Web5Did::new();
     let old_key = did.public_key.clone();
-    
-    did.rotate_keys()
-        .expect("Key rotation failed");
-    
+
+    did.rotate_keys().expect("Key rotation failed");
+
     assert_ne!(did.public_key, old_key, "DID keys not rotated");
 }
 
@@ -14,9 +13,8 @@ fn test_web5_credential_revocation() {
     let vc = VerifiableCredential::new()
         .set_id("vc:1")
         .add_revocation_list("https://revoke.anya");
-    
-    let status = vc.check_revocation()
-        .expect("Revocation check failed");
-    
+
+    let status = vc.check_revocation().expect("Revocation check failed");
+
     assert!(!status.revoked, "Credential should not be revoked");
-} 
+}
